1ee7c6b11453b0ff21c34643adbc471f7c4f2a40
chen-tianhao <fm0517@hotmail.com>
2024-06-05 17:33:00 +0800

    completed

--- python/main.py
+++ python/main.py
@@ -5,10 +5,34 @@ from datetime import datetime, timezone
 import pymysql
 
 
+def time_format_conversion(time_str):
+    time_str = time_str[:19]
+    time_format = "%Y-%m-%d %H:%M:%S"
+    time_obj = datetime.strptime(time_str, time_format)
+    return time_obj
+
+
 def pre_process_values(list_of_values):
+    for i in range(len(list_of_values)):
+        item = list_of_values[i]
+        if isinstance(item, dict):
+            if 'Month' in item and 'Day' in item and 'Hour' in item and 'Minute' in item:
+                list_of_values[i] = f"{item['Month']}/{item['Day']} {item['Hour']}:{item['Minute']}"
+            else:
+                json_str = json.dumps(item)
+                list_of_values[i] = json_str
+        elif isinstance(item, str):
+            if item.endswith('UTC'):
+                list_of_values[i] = time_format_conversion(item)
+            elif len(item) == 0:
+                list_of_values[i] = 'null'
+        elif isinstance(item, bytes):
+            list_of_values[i] = 'BinaryData'
+        elif item is None:
+            list_of_values[i] = 'None'
     # handle ETA
     # handle Dimension
-    return ""
+    return tuple(list_of_values)
 
 
 def insert_into_database(connection, json_str):
@@ -24,6 +48,8 @@ def insert_into_database(connection, json_str):
         new_key = key.lower()  # 将键转换为小写形式
         if new_key not in deduplicate_dict:
             deduplicate_dict[new_key] = value
+    if 'binarydata' in deduplicate_dict:
+        deduplicate_dict['binarydata'] = 'BinaryData'
 
     keys = deduplicate_dict.keys()
     values = deduplicate_dict.values()
@@ -35,14 +61,18 @@ def insert_into_database(connection, json_str):
             # 插入数据的 SQL 语句
             sql = "INSERT INTO {} ({})VALUES ({})".format(message_type, columns, column_space_holder)
             # 提取 JSON 数据并执行插入操作
-            cursor.execute(sql, tuple(list(values)))
+            paras = pre_process_values(list(values))
+            string_elements = [str(element) for element in paras]
+            para_string = ",".join(string_elements)
+            # print("SQL: {}\nPARAS: {}".format(sql, para_string))
+
+            cursor.execute(sql, paras)
+
             # 提交事务
             connection.commit()
-            print("Data inserted successfully")
+            print("Data inserted successfully\n")
     except Exception as e:
         print(f"Error: {e}")
-    finally:
-        connection.close()
 
 
 async def connect_ais_stream():
@@ -70,23 +100,17 @@ async def connect_ais_stream():
 
         async for message_json in websocket:
             message = json.loads(message_json)
-            # print(message_json)
-            
-            insert_into_database(connection, message_json)
-
             message_type = message["MessageType"]
+            insert_into_database(connection, message_json)
+            
             if message_type not in message_type_set:
                 message_type_set.add(message_type)
                 print("[MSG TYPE", len(message_type_set), "]", message_type)
                 print(message_json.decode('utf-8'))
-            
-            '''
-            if message_type == "PositionReport":
-                # the message parameter contains a key of the message type which contains the message itself
-                ais_message = message['Message']['PositionReport']
-                print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {ais_message['Latitude']} Longitude: {ais_message['Longitude']}")
-            '''
-        print('**********************')
+                # print('--------------------------------------\n')
+        
+        # connection.close()
+        print('**************************************')
         print(message_type_set)
 
 
